5606860ccc1a189ccd7ef0c952d7ebb5b420c591
Nick <mikabrmail@gmail.com>
2022-08-11 01:05:50 +0300

    task4 complete

---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти расстояние между точками в пространстве 2D
+Console.Write("Введите координату X точки 1: ");
+double x1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите координату Y точки 1: ");
+double y1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите координату X точки 2: ");
+double x2 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите координату Y точки 2: ");
+double y2 = double.Parse(Console.ReadLine()!);
+
+double cat1 = x2 - x1;
+double cat2 = y2 - y1;
+
+double distance = Math.Sqrt(cat1 * cat1 + cat2 * cat2);
+
+Console.WriteLine("Расстояние равно: " + distance);
