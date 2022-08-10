37d7f8b25f41d9a61cb1ce2698d43e0cb7c7c986
Nick <mikabrmail@gmail.com>
2022-08-11 01:34:45 +0300

    task5 complete

---
+++ task5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти кубы чисел от 1 до N
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int count = 1;
+
+while (count <= n)
+{
+    int coub = count * count * count;
+    // int coub = Convert.ToInt32(Math.Pow (count, 3));
+    Console.Write(coub + " ");
+    count++;
+}
