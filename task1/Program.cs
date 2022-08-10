53cf47230017c6478abbe723e205cfbb8d825755
Nick <mikabrmail@gmail.com>
2022-08-10 23:02:12 +0300

    task1 complete

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.Write("Введите номер дня недели: ");
+int numday = int.Parse(Console.ReadLine()!);
+
+int [] workday = {1, 2, 3, 4, 5};
+int [] holiday = {6, 7};
+
+if (holiday.Contains (numday))
+{
+Console.WriteLine("Это выходной день недели");
+}
+else 
+{
+    if (workday.Contains (numday))
+    {
+    Console.WriteLine("Это рабочий день недели");
+    }
+    else Console.WriteLine("Error, formatting C: start");
+}
