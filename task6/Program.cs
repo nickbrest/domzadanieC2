3478fe76f057eb9990a9f426984eb3bc9dc7a3db
Nick <mikabrmail@gmail.com>
2022-08-11 01:55:34 +0300

    task6 complete

---
+++ task6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти сумму чисел от 1 до А
+Console.Write("Введите число A: ");
+int a = int.Parse(Console.ReadLine()!);
+
+int count = 1;
+int summ = 0;
+
+while (count <= a)
+{
+    summ = summ + count;
+    count++;
+}
+Console.Write("Сумма чисел равна: ");
+Console.WriteLine(summ);
