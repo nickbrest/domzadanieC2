e2b9fe60c216da10a6580c433966bdb70bda8f68
Nick <mikabrmail@gmail.com>
2022-08-11 20:18:47 +0300

    task10 complete

---
+++ task10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+int n = 25;
+int index = 1;
+
+while (index <= n)
+{
+    int coub = index * index * index;
+    if ((coub % 10) % 2 == 0)
+    Console.Write(coub + " ");
+    index++;
+}
