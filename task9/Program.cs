6187ea6cdaa6cd22f1871f14c5351bcef3c5acfd
Nick <mikabrmail@gmail.com>
2022-08-11 19:53:06 +0300

    task9 complete

---
+++ task9/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int count = 1;
+int mult = 1;
+
+while (count <= n)
+{
+    mult = mult * count;
+    count++;
+}
+Console.Write("Произведение чисел равно: " + mult);
